e1036b610cb1887d5511e85b718ea8409c80b994
Nicolas Mitestainer <nicolas.mitestainer08@gmail.com>
2024-10-14 15:19:41 -0300

    feat: user check-ins history
    foi adicionado a funcionalidade de obter o histórico de check-ins do usuário, já sendo adicionado a regra de 20 itens por página.

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -23,6 +23,13 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     return checkInOnSameDate
   }
 
+  async findManyByUserId(userId: string, page: number) {
+    return this.items
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+    // no slice temos 2 argumentos: o primeiro seria a primeira parte, o numero de itens que queremos pegar, e o segundo argumento seria a segunda parte, o limite; no primeiro argumento pegamos o numero da pagina - 1 (pois o indice começa em 0), nosso requisito não funcional é de 20 itens por página, então fica assim: page 1 - 1 = 0, 0 * 20 = 0 (começamos a pegar desde o primeiro item), page 1 * 20 = 20, pegamos do item 0 até o item 20 do nosso array filtrado.
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
