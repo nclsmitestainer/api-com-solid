f54e459fdc71629f0a863326086aad86efd7fec4
Nicolas Mitestainer <nicolas.mitestainer08@gmail.com>
2024-10-21 18:03:42 -0300

    feat: refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -19,6 +19,7 @@ export async function authenticate(req: FastifyRequest, rep: FastifyReply) {
       password,
     })
 
+    // O sistema de refresh token funciona da seguinte forma, esse token abaixo tem a vida util de apenas 10m, como foi definido no app.ts, mas o refreshToken tem vida util de 7d, no entanto o token que o frontend tem acesso e usa é o de 10m. Quando ele expirar e o usuário tentar fazer uma requisição nova, o back-end irá verificar se ele possui um refreshToken e a partir do refresh ele irá criar um novo token acessível. Dessa forma, o usuário terá acesso por tempo ilimitado a plataforma, contanto que não fique off por 7d.
     const token = await rep.jwtSign(
       {},
       {
@@ -28,9 +29,27 @@ export async function authenticate(req: FastifyRequest, rep: FastifyReply) {
       },
     )
 
-    return rep.status(200).send({
-      token,
-    })
+    const refreshToken = await rep.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
+    )
+
+    return rep
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true, // Encriptado via HTTPS
+        sameSite: true, // Seta que ele apenas será acessivel dentro do mesmo dominio
+        httpOnly: true, // Esse cookie só será acessado pelo back-end, por requisição e resposta, somente pelo contexto da requisição, não ficará salvo ou acessivo no front-end
+      })
+      .status(200)
+      .send({
+        token,
+      })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return rep.status(400).send({ message: err.message })
---
+++ src/http/controllers/users/refresh.ts
@@ -0,0 +1,36 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function refresh(req: FastifyRequest, rep: FastifyReply) {
+  await req.jwtVerify({ onlyCookie: true })
+
+  const token = await rep.jwtSign(
+    {},
+    {
+      sign: {
+        sub: req.user.sub,
+      },
+    },
+  )
+
+  const refreshToken = await rep.jwtSign(
+    {},
+    {
+      sign: {
+        sub: req.user.sub,
+        expiresIn: '7d',
+      },
+    },
+  )
+
+  return rep
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true, // Encriptado via HTTPS
+      sameSite: true, // Seta que ele apenas será acessivel dentro do mesmo dominio
+      httpOnly: true, // Esse cookie só será acessado pelo back-end, por requisição e resposta, somente pelo contexto da requisição, não ficará salvo ou acessivo no front-end
+    })
+    .status(200)
+    .send({
+      token,
+    })
+}
